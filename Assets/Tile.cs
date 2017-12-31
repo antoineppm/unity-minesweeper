2114566a8ec8344d6fb80f21de4846a090ad0ab7
Antoine Maréchal <marechal.an@gmail.com>
2017-12-31 23:14:49 +0100

    Added reveal on left click

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileState {hidden, revealed}
+
 public class Tile : MonoBehaviour {
 	
+	[SerializeField] private Sprite hidden_sprite;
+	[SerializeField] private Sprite revealed_sprite;
+	
+	public TileState state {get; private set;}
+	
 	void Start () {
-		
+		this.state = TileState.hidden;
+		gameObject.GetComponent<SpriteRenderer>().sprite = this.hidden_sprite;
 	}
 	
 	void Update () {
 		
+	}
+	
+	void OnMouseDown() {
+		this.state = TileState.revealed;
+		gameObject.GetComponent<SpriteRenderer>().sprite = this.revealed_sprite;
 	}
 }
